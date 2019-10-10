451d392a998402f8e652c41f6c814577b9853c39
Tim de Kruijf <t.dekruijf@student.han.nl>
2019-10-10 12:32:45 +0200

    Adds playercontroller script along with playerdata as scriptable object

---
+++ SaveTheTeachers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public PlayerData playerData;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(IsInventoryItem(other.gameObject))
+        {
+            addItemToInventory(other.gameObject);
+        }
+    }   
+
+    public void addItemToInventory(GameObject item)
+    {
+        playerData.inventory.Add(item);
+    }
+
+    public bool IsInventoryItem(GameObject item)
+    {
+        return item.CompareTag("Key") || item.CompareTag("");
+    }
+
+    public bool UseKey()
+    {
+        foreach (var item in playerData.inventory)
+        {
+            if(item.CompareTag("Key"))
+            {
+                playerData.inventory.Remove(item);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ SaveTheTeachers/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public int health;
+    public List<GameObject> inventory = new List<GameObject>();
+    public bool fusionAchieved;
+    public bool jeffRescued;
+}
