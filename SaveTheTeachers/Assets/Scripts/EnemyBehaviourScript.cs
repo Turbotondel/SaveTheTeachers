f792fd4ae194dd9aa62c1a4bb02ac9db7f8578f6
RyyzQ <ryyzqq@gmail.com>
2019-10-10 12:48:30 +0200

    Replaced old script with one working only for navmesh

---
+++ SaveTheTeachers/Assets/Scripts/EnemyBehaviourScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject EnemyPath;
+    [SerializeField]
+    List<Transform> points = new List<Transform>();
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        foreach (Transform child in EnemyPath.transform)
+        {
+            if (child != EnemyPath.transform)
+                points.Add(child);
+        }
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Count == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Count;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
